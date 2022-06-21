8a9df88bd1e3cf2c40150759dc43ebfaf99f0413
Sumeet Wagh <sumeetwagh70@gmail.com>
2022-06-21 16:18:18 +0530

    first commit

---
+++ windows/RNWithoutExpo/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RNWithoutExpo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RNWithoutExpo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
